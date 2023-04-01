1f7382dbf17475ebd7f751ca63cd12c6345ccdd5
Solijonov Qodirjon <solijonovqodirjon@gmail.com>
2023-04-01 10:30:19 +0500

    minor changes

--- src/views/HomeView/HomeBanner/BannerItem.tsx
+++ src/views/HomeView/HomeBanner/BannerItem.tsx
@@ -1,10 +1,8 @@
 import React, { FC } from 'react';
 
-// import { Container } from '@/components/Container';
-
 import { IBannerItemProps } from './BannerItem.type';
+
 import styles from './BannerItem.module.scss';
-// import { Text } from '@/components/Text';
 
 export const BannerItem: FC<IBannerItemProps> = ({ imgSrc }) => {
   return (
